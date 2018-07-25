4ffbf5883849e6cde8b25bab6c57fda37382ad52
ig3xy <kirazv@debian>
2018-07-25 15:05:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+$(function() {
+
+	$('.tab-panels .tabs li').on('click', function(){
+
+		var panelToShow = $(this).attr('rel');
+
+
+
+		$('.tab-panels .panel.active').hide(200, function(){
+			$(this).removeClass('active');
+
+			$('#'+panelToShow).show(200, function(){
+				$(this).addClass('active');
+				});
+		});
+
+	});
+});
+
+
+// wysuwa "kategorie"
+
+$(document).ready(function(){
+	$("#dropkat").click(function(){
+		$("#pkat").slideToggle("fast");
+		
+	});
+});
+
+
+// do zamknięcia "kategorii"
+
+$(function(){
+	$("#x").on('click', function(){
+		$("#zamknij").hide();
+	});
+});
+
+$(function(){
+	$("#x").on('click', function(){
+		$(this).hide();
+	});
+});
+
+
+
+$(function(){
+	$("#dropkat").on('click', function(){
+		$("#zamknij").show();
+		$("#x").show();				
+	});
+});
+
+
+
+
+$(function(){
+	$(".tab-panels .tabs li").on('click', function(){
+		$(".media").hide();
+		$(".recap").hide();
+		$(".recap2").hide();
+	});
+});
+
+$(function(){
+	$(".main").on('click', function(){
+		$(".media").show();
+		$(".recap").show();
+		$(".recap2").show();
+	});
+});
